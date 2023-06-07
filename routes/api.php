8f4a6e9ccb82284eb58a7ef643061ee9bc6e52bd
yash raj mishra <yash.raj2020@vitbhopal.ac.in>
2023-06-08 00:29:52 +0530

    new commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\bussearch;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post("input",[bussearch::class,"sendData"])->name('inputValue');
