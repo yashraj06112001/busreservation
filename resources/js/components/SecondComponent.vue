82df666c1061b6c23a131702a8bd9b4cfb673ee8
yash raj mishra <yash.raj2020@vitbhopal.ac.in>
2023-06-10 22:04:35 +0530

    second component extension

--- resources/js/components/SecondComponent.vue
+++ resources/js/components/SecondComponent.vue
@@ -7,7 +7,8 @@ export default  {
     {
         return {
             available:"",
-            available_time:""
+            available_time:"",
+            waiting:""
         }
     }
         
