8a3f0c40cdd2e17ba3c0e1d5ee3df6710f1b4183
yash raj mishra <yash.raj2020@vitbhopal.ac.in>
2023-06-10 21:57:39 +0530

    extending second component

--- resources/js/components/SecondComponent.vue
+++ resources/js/components/SecondComponent.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>second component</h1>
 </template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+<style scoped>
+
+</style>
