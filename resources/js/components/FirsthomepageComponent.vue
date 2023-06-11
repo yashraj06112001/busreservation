34a6bdde0c1aa3cfede99a089c1b4a08ba28077b
yash raj mishra <yash.raj2020@vitbhopal.ac.in>
2023-06-12 02:37:01 +0530

    new commit

--- resources/js/components/FirsthomepageComponent.vue
+++ resources/js/components/FirsthomepageComponent.vue
@@ -4,7 +4,20 @@
     </div>
 </template>
 <script>
+import FirstComponent from './FirstComponent.vue';
+export default{
 
+    data:function()
+    {
+        return {
+            data:""
+        }
+    },
+    components:{
+        FirstComponent
+    }
+    
+}
 </script>
 <style scoped>
 
